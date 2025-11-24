e9d16cfcdc66606f26fe2c7f2c83b81582b9e01a
leo <leonardoelbaneado@gmail.com>
2025-11-23 19:17:36 -0500

    quizz 1

--- src/pages/quizz/lesson1Quizz/Lesson1Quizz1.tsx
+++ src/pages/quizz/lesson1Quizz/Lesson1Quizz1.tsx
@@ -1,9 +1,28 @@
 import OneSelect from '@/components/quizz/OneSelect'
+import CodeBlock from '@/components/CodeBlock'
 export default function Lesson1Quizz1() {
   const opciones = {
     nivel: 'Quizz 1',
-    pregunta: '¿Capital del perú?',
+    pregunta: '¿Donde esta el error?',
+    codigo: (
+      <CodeBlock>{
+        /*html*/ `function Productos() {
+  const productos = [ { id: 1, nombre: "Laptop", precio: 1200 },
+    { id: 2, nombre: "Mouse", precio: 25 },
+  ];
 
+  return (
+    <ul>
+      {productos.map((producto) => (
+        <li key={producto.id}>
+          {producto.nombre} - {producto.precio}
+        </li>
+      ))}
+    </ul>
+  );
+}`
+      }</CodeBlock>
+    ),
     opciones: [
       { clave: 'a', texto: 'Roma' },
       { clave: 'b', texto: 'Madrid' },
